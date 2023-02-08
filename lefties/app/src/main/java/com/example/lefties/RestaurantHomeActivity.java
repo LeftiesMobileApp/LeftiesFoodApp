c9956c64c1d2ef00b81728579b3b3cbe2b00deff
maccimac <macci.web@gmail.com>
2023-02-07 21:06:36 -0800

    Add colors and logos

---
+++ lefties/app/src/main/java/com/example/lefties/RestaurantHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.lefties;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_home);
+    }
+}
