6f8a50255618ef9ed4dfea4a6556e3e0de0586ed
RaiyanKhalil <raiyankhalil@gmail.com>
2023-03-14 17:35:24 -0700

    fixed login screen

--- lefties/app/src/main/java/com/example/lefties/MainActivity.java
+++ lefties/app/src/main/java/com/example/lefties/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button btnLogin = findViewById(R.id.loginBtn);
         CheckBox checkBoxRestaurant = findViewById(R.id.checkboxRestaurant);
